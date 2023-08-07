d1b56b41267bbb7cdb00f3a580a7e868adf66830
bulatvk <bulatvk@gmail.com>
2023-08-08 01:13:19 +0500

    minor change

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 'use strict';
-import {clear,checkInput,calc} from './module.js';
 
-export const inputA = document.getElementById('inputA');
-export const inputB = document.getElementById('inputB');
-export const selectOperator = document.getElementById('selectOperator');
-export const equalButton = document.getElementById('equalButton');
-export const result = document.getElementById('result');
+const inputA = document.getElementById('inputA');
+const inputB = document.getElementById('inputB');
+const selectOperator = document.getElementById('selectOperator');
+const equalButton = document.getElementById('equalButton');
+const result = document.getElementById('result');
 const clearButton = document.getElementById('clearButton');
-export const memory = document.getElementById('memory');
-export const memoryText = document.getElementById('memoryText');
+const memory = document.getElementById('memory');
 
+let count = 0;
+let newDiv;
+let checkSolution;
 equalButton.style.display = 'none';
-memory.innerHTML = '';
+memory.textContent = '';
 
-clear()
+clear();
 
 inputA.addEventListener('input', () => {
 	checkInput(inputA, inputB);
@@ -32,3 +33,66 @@ clearButton.addEventListener('click', () => {
 })
 
 equalButton.addEventListener('click', calc);
+
+function clear() {
+	inputA.value = '';
+	inputB.value = '';
+	result.textContent = '';
+	equalButton.style.display = 'none';
+}
+
+function checkInput(arg1, arg2) {
+	if (!arg1.value) {
+		result.style.color = 'red';
+		result.textContent = 'Введите число!';
+	}
+	if (arg1.value === '' || arg2.value === '' || !arg1.value || !arg2.value) {
+		equalButton.style.display = 'none';
+	} else {
+		equalButton.style.display = 'inline';
+		result.textContent = '';
+	}
+}
+
+function calc() {
+	let numA = parseFloat(inputA.value);
+	let numB = parseFloat(inputB.value);
+	let operator = selectOperator.value;
+	let calcResult;
+	let calculation = new Function('numA', 'numB', `return numA ${operator} numB`);
+	calcResult = calculation(numA, numB);
+
+	inputA.value = numA;
+	inputB.value = numB;
+
+	if (!memory.textContent) {
+		memory.innerHTML = '<p id="memoryText" >Результаты вычислений:</p>';
+		memory.title = 'Нажмите чтобы очистить результаты вычислений';
+		document.getElementById('memoryText').onclick = function () {
+			memory.innerHTML = '';
+			clear()
+		}
+	}
+
+	if (numB === 0 && selectOperator.value === '/') {
+		result.style.color = 'red';
+		result.textContent = 'Ошибка! Делить на ноль нельзя!';
+	} else {
+		let solution = `${numA} ${operator} ${numB} = ${calcResult}`;
+		solution = solution.replace('*', 'x').replace('/', ':');
+		solution = solution.replace(',', '.').replace(',', '.');
+		result.style.color = 'black';
+		result.textContent = calcResult;
+		if (checkSolution !== solution) {
+			count++;
+			newDiv = document.createElement('div');
+			newDiv.id = String(count);
+			memory.appendChild(newDiv);
+			newDiv.innerHTML = solution;
+			checkSolution = solution;
+			newDiv.addEventListener("click", function (event) {
+				this.remove();
+			});
+		}
+	}
+}
